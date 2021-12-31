92a23b55c3d37405e60012223cd751a1e2c022cd
Thunyatorn Kongmoung <Thunyatorn1125@gmail.com>
2021-12-31 14:31:32 +0700

    add Thunyalab15_1complete.cpp

---
+++ Thunyalab15_1complete.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void insertionSort(T d[],int N){
+	for(int i = 1 ; i < N; i++){
+		for(int j = 0; j < i; j++){
+			if(d[i] > d[j]){
+				T temp = d[i];
+				d[i] = d[j];
+				d[j] = temp;
+			}
+		}
+	 cout << "Pass " << i << ":";
+		for (int k = 0; k < N ; k++){
+            cout << d[k] << " ";
+        }
+	 cout << "\n";
+	}
+}
+
+int main(){
+	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+	cout << "Input Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";
+	cout << "\n\n";
+	
+	insertionSort(a,10);
+	
+	cout << "\nSorted Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+}
